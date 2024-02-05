d4b2fed37feb7bf1282b54bc0423c65734df700e
Aqil Gifari <aqilgifari0@gmail.com>
2024-02-05 14:16:55 +0700

    Add margin

--- components/Card.js
+++ components/Card.js
@@ -1,24 +1,28 @@
 import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
 
-export default function Card({title, description}) {
+export default function Card({ title, description }) {
   const [isOnCart, setIsOnCart] = useState(false);
   return (
-      <View style={styles.cardContainer}>
-        <Image
-          source={{
-            uri: "https://i.pinimg.com/originals/b9/c4/7e/b9c47ef70bff06613d397abfce02c6e7.jpg",
-          }}
-          style={styles.cardImage}
-        />
+    <View style={styles.cardContainer}>
+      <Image
+        source={{
+          uri: "https://i.pinimg.com/originals/b9/c4/7e/b9c47ef70bff06613d397abfce02c6e7.jpg",
+        }}
+        style={styles.cardImage}
+      />
+      <View style={{ marginBottom: 10 }}>
         <Text>{title}</Text>
         <Text>{description}</Text>
-        <TouchableOpacity onPress={() => setIsOnCart(!isOnCart)}>
-          <View style={{ backgroundColor: "blue" }}>
-            <Text style={{ textAlign: "center", color: "white" }}>{isOnCart ? "Hapus dari Keranjang" : "Tambah ke keranjang"}</Text>
-          </View>
-        </TouchableOpacity>
       </View>
+      <TouchableOpacity onPress={() => setIsOnCart(!isOnCart)}>
+        <View style={{ backgroundColor: "blue" }}>
+          <Text style={{ textAlign: "center", color: "white" }}>
+            {isOnCart ? "Hapus dari Keranjang" : "Tambah ke keranjang"}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
   );
 }
 
@@ -30,5 +34,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   cardContainer: { backgroundColor: "white", borderRadius: 10, padding: 10 },
-  cardImage: { width: 200, height: 200, borderRadius: 10 }
+  cardImage: { width: 200, height: 200, borderRadius: 10 },
 });
