4dff8f226692e23d0d24085a82435817376e2a94
Aqil Gifari <aqilgifari0@gmail.com>
2024-02-05 14:13:37 +0700

    Add card

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import Card from "./components/Card";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Card title={"Kucing 1"} description={"Kucing 1 desc"} />
+      <Card title={"Kucing 2"} description={"Kucing 2 desc"} />
     </View>
   );
 }
@@ -13,8 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#D7D7D7",
+    alignItems: "center",
+    justifyContent: "center",
+    rowGap: 10
   },
 });
---
+++ components/Card.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
+
+export default function Card({title, description}) {
+  const [isOnCart, setIsOnCart] = useState(false);
+  return (
+      <View style={styles.cardContainer}>
+        <Image
+          source={{
+            uri: "https://i.pinimg.com/originals/b9/c4/7e/b9c47ef70bff06613d397abfce02c6e7.jpg",
+          }}
+          style={styles.cardImage}
+        />
+        <Text>{title}</Text>
+        <Text>{description}</Text>
+        <TouchableOpacity onPress={() => setIsOnCart(!isOnCart)}>
+          <View style={{ backgroundColor: "blue" }}>
+            <Text style={{ textAlign: "center", color: "white" }}>{isOnCart ? "Hapus dari Keranjang" : "Tambah ke keranjang"}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#D7D7D7",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cardContainer: { backgroundColor: "white", borderRadius: 10, padding: 10 },
+  cardImage: { width: 200, height: 200, borderRadius: 10 }
+});
