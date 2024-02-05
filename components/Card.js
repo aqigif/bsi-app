161c31c5e96cbaddb56c8ca5e3e89a87c6f2daef
Aqil Gifari <aqilgifari0@gmail.com>
2024-02-05 15:13:12 +0700

    Add Function

--- components/Card.js
+++ components/Card.js
@@ -2,7 +2,29 @@ import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
 
 export default function Card({ title, description, image }) {
-  const [isOnCart, setIsOnCart] = useState(false);
+  const [hide, setHide] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // IF ELSE => 
+  // IF (condition){
+  //    return(jsx)
+  // }
+  // return (jsx)
+  //
+  // TERNARY ? :
+  // TERNARY &&
+
+  if (hide) {
+    return (
+      <TouchableOpacity onPress={() => setHide(!hide)}>
+        <View style={{ backgroundColor: "blue" }}>
+          <Text style={{ textAlign: "center", color: "white" }}>
+            Sembunyikan Card
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
   return (
     <View style={styles.cardContainer}>
       <Image
@@ -15,13 +37,23 @@ export default function Card({ title, description, image }) {
         <Text>{title}</Text>
         <Text>{description}</Text>
       </View>
-      <TouchableOpacity onPress={() => setIsOnCart(!isOnCart)}>
+      <TouchableOpacity onPress={() => setHide(!hide)}>
         <View style={{ backgroundColor: "blue" }}>
           <Text style={{ textAlign: "center", color: "white" }}>
-            {isOnCart ? "Hapus dari Keranjang" : "Tambah ke keranjang"}
+            Munculin Card
           </Text>
         </View>
       </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => setIsLoggedIn(!isLoggedIn)}
+      >
+        <View style={{ backgroundColor: isLoggedIn ? "green" : "red" }}>
+          <Text style={{ textAlign: "center", color: "white" }}>
+            {isLoggedIn ? "Sudah Login" : "Belum Login"}
+          </Text>
+        </View>
+      </TouchableOpacity>
+      {isLoggedIn && <Text>Sudah masuk</Text>}
     </View>
   );
 }
